951d7239ea643ca345affa3f1e10247120afb1e2
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-17 13:54:24 +0100

    Created functions to handle submit

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
-  function search() {
-    alert("Searching...");
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
   }
 
   return (
     <div className="Search">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} />
+        <input type="search" onChange={handleKeywordChange} autoFocus={true} />
       </form>
     </div>
   );

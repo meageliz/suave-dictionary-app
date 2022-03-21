26cd8955883bba70befe03fa61f00f462305c04c
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-21 17:22:33 +0100

    Finished building out Meaning comp

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,8 +4,17 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>{props.meaning.definitions[0].example}</p>
+      {props.meaning.definition.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }

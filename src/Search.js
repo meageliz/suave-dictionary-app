c2f744d2e4ce6dad5ff09d2b6b6f3f2089d75f73
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-24 12:30:17 +0100

    Created photo component

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Search.css";
 
 export default function Search(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
   function handlePexelsResponse(response) {
-    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function search() {
@@ -55,6 +57,7 @@ export default function Search(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {

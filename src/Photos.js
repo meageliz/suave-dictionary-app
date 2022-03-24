dc245be6f0ce84c5c10a9653d1f226d647d57a2c
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-24 14:00:24 +0100

    Fixed bug maybe?

--- src/Photos.js
+++ src/Photos.js
@@ -13,7 +13,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt-prop="photos"
+                    alt="photos"
                   />
                 </a>
               </div>
